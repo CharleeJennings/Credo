db92a4ae9c2d5fdb31c2208155f22fb1084b9a82
Charles Jennings <charlesjennings@Charless-MacBook-Pro.local>
2019-10-29 16:48:20 -0500

    Update to tab bar with color

---
+++ components/Connect.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+export default Connect()
+
+
+{
+    return (
+
+
+        <View> 
+
+        </View>
+    ) 
+}
--- components/Navigation.js
+++ components/Navigation.js
@@ -1,19 +1,46 @@
-import { createAppContainer } from 'react-navigation';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';  // Container for application
+
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import React from 'react'
 import Quote from './Quote'
-import Settings from './Settings'
+import Settings from './Settings'  
+import { Icon } from 'react-native-elements' /// Icon set check API for more information 
 
 
 
 
+///////////////////
+/////// This is a functions to return the icon when focused on the tab and in general the icons 
 
+const getTabBarIcon = (navigation, focused, tintColor) => {
+    const { routeName } = navigation.state;
+    let iconName;
+    if (routeName === 'Home') {
+      iconName = `chat-bubble${focused ? '' : '-outline'}`;
+      // We want to add badges to home tab icon
+
+    } else if (routeName === 'Settings') {
+      iconName = `notifications${focused ? '' : '-none'}`;
+    }
+  
+    // You can return any component that you like here!
+    return <Icon name={iconName} size={25}  type='material' color={tintColor} />;
+  };
 
 const TabNavigator = createBottomTabNavigator({
     Home: Quote,
     Settings: Settings,
-  });
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+        tabBarIcon: ({ focused, tintColor }) =>
+          getTabBarIcon(navigation, focused, tintColor),
+      }),
+      tabBarOptions: {
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      },
+    });
   
 
 
