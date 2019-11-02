ed532fcb276da0d04bfb4652c00745dac32ec253
Lina King <maelinaking@gmail.com>
2019-11-02 15:30:55 -0500

    connecting quotes to credo db

--- firebase.js
+++ firebase.js
@@ -2,15 +2,17 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB5ka7C-M-SUOT650seTZiFnQ_A0clYHC4",
-    authDomain: "credo-local.firebaseapp.com",
-    databaseURL: "https://credo-local.firebaseio.com",
-    projectId: "credo-local",
-    storageBucket: "credo-local.appspot.com",
-    messagingSenderId: "388403717742",
-    appId: "1:388403717742:web:1a6038d7a773528a75e87f"
-  };
-  // Initialize Firebase
-  const flamebase = firebase.initializeApp(firebaseConfig);
+    apiKey: "AIzaSyBr3T5xUwClPmzc5Xx3cPDiKEGRN8Tskis",
+    authDomain: "credo-d6c6b.firebaseapp.com",
+    databaseURL: "https://credo-d6c6b.firebaseio.com",
+    projectId: "credo-d6c6b",
+    storageBucket: "credo-d6c6b.appspot.com",
+    messagingSenderId: "1050656781573",
+    appId: "1:1050656781573:web:bb7075360a1d84a3ad336c",
+    measurementId: "G-167F19728D"
+};
 
-  export default flamebase;
+// Initialize Firebase
+const flamebase = firebase.initializeApp(firebaseConfig);
+
+export default flamebase;
